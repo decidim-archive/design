fe9f588f16c21a8534f773c6ee278fdaaebc9910
Javier Usobiaga <javier@swwweet.com>
2016-09-08 12:32:24 +0200

    phase nav functionality

--- source/javascripts/body.js
+++ source/javascripts/body.js
@@ -3,10 +3,28 @@
 //= require foundation-sites/dist/foundation.min
 
 //= require appendAround.js
+
 //= require map/marker
 //= require map/map
 
+//= require owl.carousel.min.js
+
 $(document).foundation();
+
 
 $(".js-append").appendAround();
 
+
+//Owl carousel for phase nav
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    pagination: false
+  });
+  var owl = $(".owl-carousel");
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  });
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  });
+});
